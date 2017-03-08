f3029cb5c2366923daff346232609e9efac1238e
magicindustries <toby@magicindustries.net>
2017-03-08 21:04:31 +1100

    labels, milestones, issues working

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 var GitHubAPI = require('github');
-var issues = require('../data/issues.json');
 var labels = require('../data/labels.json');
+var issues = require('../data/issues.json');
+var milestones = require('../data/milestones.json');
 var apiDelay = 1000;
 
 var repoOwner = 'LithodomosVR';
@@ -29,9 +30,11 @@ gh.issues.getLabels({
   repo: repoName,
   per_page: 100
 }).then(function (_labels) {
-  var promiseStack = [];
+  console.log('blah00')
+  var deletePromises = [];
   _labels.data.forEach(function (item) {
-    promiseStack.push(
+    console.log('blah01')
+    deletePromises.push(
       gh.issues.deleteLabel({
         owner: repoOwner,
         repo: repoName,
@@ -40,10 +43,13 @@ gh.issues.getLabels({
         console.log('Error: ', error);
       }));
   });
-  Promise.all(promiseStack).then(function () {
-    var promiseStack2 = [];
+  Promise.all(deletePromises).then(function () {
+    console.log('blah02')
+    var labelPromises = [];
     labels.forEach(function (item) {
+      console.log('blah03')
       var func = function (_item) {
+        console.log('blah04')
         return function () {
           console.log(_item)
           return gh.issues.createLabel({
@@ -56,15 +62,107 @@ gh.issues.getLabels({
           });
         }
       }(item);
-      promiseStack2.push(func)
-    })
-    promiseStack2.reduce(function (promise, thing) {
+      labelPromises.push(func);
+    });
+    var labelPromise = labelPromises.reduce(function (promise, thing) {
       return promise.then(function(result) {
         return thing().then(function (result) {
           return result;
         });
       });
     }, Promise.resolve())
+
+    labelPromise.then(function () {
+      console.log('blah1')
+      gh.issues.getMilestones({
+        owner: repoOwner,
+        repo: repoName,
+        per_page: 100
+      }).then(function (_milestones) {
+        console.log('blah2')
+        var deleteMSPromises = [];
+        _milestones.data.forEach(function (milestone) {
+          console.log('blah3')
+          deleteMSPromises.push(
+            gh.issues.deleteMilestone({
+              owner: repoOwner,
+              repo: repoName,
+              number: milestone.number
+            })
+          )
+        });
+        Promise.all(deleteMSPromises).then(function (result) {
+          console.log('blah4')
+          var milestonePromises = [];
+          milestones.forEach(function (item) {
+            console.log('blah5')
+            var func = function (_item) {
+              return function () {
+                console.log(_item)
+                return gh.issues.createMilestone({
+                  owner: repoOwner,
+                  repo: repoName,
+                  title: item.title,
+                  state: item.state,
+                  description: item.description,
+                  due_on: item.due_on || new Date()
+                }).then(function (result) {
+                  return result;
+                }).catch (function (error) {
+                  console.log('Error: ', error);
+                });
+              }
+            }(item);
+            milestonePromises.push(func);
+          });
+          var milestonePromise = milestonePromises.reduce(function (promise, thing) {
+            return promise.then(function(result) {
+              return thing().then(function (result) {
+                return result;
+              });
+            });
+          }, Promise.resolve());
+
+          milestonePromise.then(function () {
+            console.log('blah4')
+            var issuePromises = [];
+            issues.forEach(function (item) {
+              console.log('blah5')
+              var func = function (_item) {
+                return function () {
+                  console.log(_item)
+                  return gh.issues.create({
+                    owner: repoOwner,
+                    repo: repoName,
+                    title: item.title,
+                    state: item.state,
+                    description: item.description,
+                    due_on: item.due_on || new Date()
+                  }).then(function (result) {
+                    return result;
+                  }).catch (function (error) {
+                    console.log('Error: ', error);
+                  });
+                }
+              }(item);
+              issuePromises.push(func)
+            })
+            var issuePromise = issuePromises.reduce(function (promise, thing) {
+              return promise.then(function(result) {
+                return thing().then(function (result) {
+                  return result;
+                });
+              });
+            }, Promise.resolve());
+
+            issuePromise.then(function () {
+
+            });
+
+          });
+        });
+      });
+    });
   });
 });
 
