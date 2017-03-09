7051e73843771ba762b0733ca80efc9eaf61ad7b
magicindustries <toby@magicindustries.net>
2017-03-09 16:14:17 +1100

    extracted config, removed old oauth token

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ var GitHubAPI = require('github');
 var labels = require('../data/labels.json');
 var issues = require('../data/issues.json');
 var milestones = require('../data/milestones.json');
-
-var repoOwner = 'LithodomosVR';
-var repoName = "testproject";
+var config = require('../config');
 
 var gh = new GitHubAPI({
   debug: false,
@@ -20,13 +18,13 @@ var gh = new GitHubAPI({
 
 var temp = gh.authenticate({
     type: "oauth",
-    token: "1fae32648d7a8a9f593ce2b63aa3e6d33b7bbae0"
+    token: config.oAuthToken
 });
 
 // remove all current repo labels
 gh.issues.getLabels({
-  owner: repoOwner,
-  repo: repoName,
+  owner: config.repoOwner,
+  repo: config.repoName,
   per_page: 100
 }).then(function (_labels) {
 
@@ -37,8 +35,8 @@ gh.issues.getLabels({
     // delete the label
     deletePromises.push(
       gh.issues.deleteLabel({
-        owner: repoOwner,
-        repo: repoName,
+        owner: config.repoOwner,
+        repo: config.repoName,
         name: item.name
       }).catch (function (error) {
         console.log('Error: ', error);
@@ -55,8 +53,8 @@ gh.issues.getLabels({
       var func = function (_item) {
         return function () {
           return gh.issues.createLabel({
-            owner: repoOwner,
-            repo: repoName,
+            owner: config.repoOwner,
+            repo: config.repoName,
             name: item.name,
             color: item.color,
           }).then(function (result) {
@@ -82,8 +80,8 @@ gh.issues.getLabels({
 
       // grab any current milestones
       gh.issues.getMilestones({
-        owner: repoOwner,
-        repo: repoName,
+        owner: config.repoOwner,
+        repo: config.repoName,
         per_page: 100
       }).then(function (_milestones) {
 
@@ -94,8 +92,8 @@ gh.issues.getLabels({
           // delete the milestone
           deleteMSPromises.push(
             gh.issues.deleteMilestone({
-              owner: repoOwner,
-              repo: repoName,
+              owner: config.repoOwner,
+              repo: config.repoName,
               number: milestone.number
             })
           )
@@ -110,8 +108,8 @@ gh.issues.getLabels({
             var func = function (_item) {
               return function () {
                 return gh.issues.createMilestone({
-                  owner: repoOwner,
-                  repo: repoName,
+                  owner: config.repoOwner,
+                  repo: config.repoName,
                   title: item.title,
                   state: item.state,
                   description: item.description,
@@ -148,8 +146,8 @@ gh.issues.getLabels({
                 // create the issue
                 return function () {
                   return gh.issues.create({
-                    owner: repoOwner,
-                    repo: repoName,
+                    owner: config.repoOwner,
+                    repo: config.repoName,
                     title: item.title,
                     state: item.state,
                     description: item.description,
