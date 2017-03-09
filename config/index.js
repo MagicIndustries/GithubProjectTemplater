b1ea45b6f5587a5b97d5c17f53c2a7ef544fa183
magicindustries <toby@magicindustries.net>
2017-03-09 16:15:49 +1100

    removed old oauth token

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
   repoOwner: "[INSERT GITHUB USERNAME]",
   repoName: "[INSERT GITHUB REPO NAME]",
-  oAuthToken: "1fae32648d7a8a9f593ce2b63aa3e6d33b7bbae0"
+  oAuthToken: "[INSERT GITHUB ACCESS TOKEN]"
 }
